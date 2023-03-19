9f328125a0361047c5a934d58ba0ae24333d661a
GenryEden <a926788@gmail.com>
2023-03-19 17:42:49 +0300

    made all main parts

---
+++ app/src/main/java/com/example/flocator/settings/ChangePasswordFragment.kt
@@ -0,0 +1,36 @@
+package com.example.flocator.settings
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.FrameLayout
+import com.example.flocator.R
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.google.android.material.button.MaterialButton
+
+
+class ChangePasswordFragment : BottomSheetDialogFragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val fragmentView = inflater.inflate(R.layout.fragment_change_password, container, false)
+        val confirmButton = fragmentView.findViewById<MaterialButton>(R.id.change_pass_confirm_button)
+        val closeButton = fragmentView.findViewById<FrameLayout>(R.id.change_password_close_button)
+        confirmButton.setOnClickListener {
+            dismiss()
+        }
+        closeButton.setOnClickListener {
+            dismiss()
+        }
+        return fragmentView
+    }
+
+    companion object {
+        const val TAG = "Change pass fragment"
+    }
+
+
+}
--- app/src/main/java/com/example/flocator/settings/PrivacySettings.kt
+++ app/src/main/java/com/example/flocator/settings/PrivacySettings.kt
@@ -6,27 +6,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.FrameLayout
+import android.widget.LinearLayout
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.flocator.R
-import com.example.flocator.databinding.FragmentPrivacySettingsBinding
 
 
 
-/**
- * A simple [Fragment] subclass.
- * Use the [PrivacySettings.newInstance] factory method to
- * create an instance of this fragment.
- */
 class PrivacySettings : Fragment() {
-    private lateinit var binding: FragmentPrivacySettingsBinding
     private lateinit var friendListAdapter: FriendListAdapter
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
 
-    }
-
-    fun getFriend(): ArrayList<Friend> {
+    fun getFriends(): ArrayList<Friend> {
         val ans = ArrayList<Friend>()
         for (i in 1..10000) {
             ans.add(
@@ -49,17 +39,33 @@ class PrivacySettings : Fragment() {
         val recyclerView = fragmentView.findViewById<RecyclerView>(R.id.friend_list)
         val backButton = fragmentView.findViewById<FrameLayout>(R.id.back_button)
         val selectAllButton = fragmentView.findViewById<FrameLayout>(R.id.select_all_frame)
+        val currentLocation = fragmentView.findViewById<LinearLayout>(R.id.privacy_current_location)
+        val approxLocation = fragmentView.findViewById<LinearLayout>(R.id.privacy_approx_location)
+        val fixedLocation = fragmentView.findViewById<LinearLayout>(R.id.privacy_fix_location)
         backButton.setOnClickListener {
             requireActivity().onBackPressedDispatcher.onBackPressed()
         }
         recyclerView.layoutManager = GridLayoutManager(context, 3)
 
-        friendListAdapter = FriendListAdapter(getFriend())
+        friendListAdapter = FriendListAdapter(getFriends())
         recyclerView.adapter = friendListAdapter
 
         selectAllButton.setOnClickListener {
             friendListAdapter.selectAll()
         }
+
+        currentLocation.setOnClickListener {
+            friendListAdapter.unselectAll()
+        }
+
+        approxLocation.setOnClickListener {
+            friendListAdapter.unselectAll()
+        }
+
+        fixedLocation.setOnClickListener {
+            friendListAdapter.unselectAll()
+        }
+
 
         return fragmentView
     }
