0f59de980886106f845f8fae9c060d56871b56e3
radkoff26 <radko.slava@mail.ru>
2023-03-24 10:12:35 +0300

    add bordered rounded image view

--- app/src/main/java/com/example/flocator/main/views/MapFriendView.kt
+++ app/src/main/java/com/example/flocator/main/views/MapFriendView.kt
@@ -4,7 +4,13 @@ import android.content.Context
 import android.graphics.Bitmap
 import android.util.AttributeSet
 import android.util.DisplayMetrics
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.FrameLayout
+import android.widget.ImageView
+import android.widget.LinearLayout
 import com.example.flocator.R
+import com.example.flocator.databinding.FriendMapViewBinding
 import com.google.android.material.imageview.ShapeableImageView
 import com.google.android.material.shape.RoundedCornerTreatment
 import com.google.android.material.shape.ShapeAppearanceModel
@@ -15,32 +21,35 @@ class MapFriendView @JvmOverloads constructor(
     attrs: AttributeSet? = null,
     defStyleAttr: Int = 0,
     bitmap: Bitmap? = null
-) : ShapeableImageView(context, attrs, defStyleAttr) {
+) : FrameLayout(context, attrs, defStyleAttr) {
     private val size = dpToPx(48)
+    private val padding = dpToPx(2)
+    private val fullSize = size + padding * 2
+    private var shapeableImageView: ShapeableImageView
     var mBitmap: Bitmap? = bitmap
         set(value) {
             if (value != null) {
-                setImageBitmap(value)
+                shapeableImageView.setImageBitmap(value)
             }
         }
 
     init {
-        this.shapeAppearanceModel = ShapeAppearanceModel.builder()
-            .setAllCorners(RoundedCornerTreatment())
-            .setAllCornerSizes(size.toFloat() / 2)
-            .build()
-
-        scaleType = ScaleType.CENTER_CROP
-
-        if (bitmap == null) {
-            setBackgroundResource(R.drawable.circle_bg)
-        } else {
-            setImageBitmap(bitmap)
-        }
-    }
-
-    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
-        setMeasuredDimension(size, size)
+        LayoutInflater.from(context).inflate(R.layout.friend_map_view, this, true)
+        shapeableImageView = FriendMapViewBinding.bind(this).image
+//        shapeableImageView.shapeAppearanceModel = ShapeAppearanceModel.builder()
+//            .setAllCorners(RoundedCornerTreatment())
+//            .setAllCornerSizes(size.toFloat() / 2)
+//            .build()
+//
+//        shapeableImageView.scaleType = ImageView.ScaleType.CENTER_CROP
+//        shapeableImageView.strokeWidth = padding.toFloat()
+//        shapeableImageView.strokeColor = resources.getColorStateList(R.color.tint, null)
+//
+//        setBackgroundResource(R.drawable.circle_bg)
+//
+//        if (bitmap != null) {
+//            shapeableImageView.setImageBitmap(bitmap)
+//        }
     }
 
     private fun dpToPx(dp: Int): Int {
