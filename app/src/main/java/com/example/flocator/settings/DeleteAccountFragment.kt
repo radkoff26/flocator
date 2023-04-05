bc336b8b82007c1680419e41a6b1af44e845ff97
GenryEden <a926788@gmail.com>
2023-04-05 16:13:57 +0300

    fixed markup

--- app/src/main/java/com/example/flocator/settings/DeleteAccountFragment.kt
+++ app/src/main/java/com/example/flocator/settings/DeleteAccountFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.FrameLayout
+import android.widget.ImageView
 import com.example.flocator.R
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.google.android.material.button.MaterialButton
@@ -17,13 +18,13 @@ class DeleteAccountFragment : BottomSheetDialogFragment() {
     ): View? {
         // Inflate the layout for this fragment
         val fragmentView = inflater.inflate(R.layout.fragment_delete_account, container, false)
-        val closeButton = fragmentView.findViewById<FrameLayout>(R.id.delete_account_close_button)
+        val closeButton = fragmentView.findViewById<ImageView>(R.id.delete_account_close_button)
         val confirmButton = fragmentView.findViewById<MaterialButton>(R.id.delete_account_confirm_button)
 
         closeButton.setOnClickListener {
             dismiss()
         }
-
+2
         confirmButton.setOnClickListener {
             exitProcess(-1) // временная заглушка
         }
