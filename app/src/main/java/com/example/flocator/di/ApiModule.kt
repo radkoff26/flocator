2063f02c2675ab35e86bb341608fcecda8e351a6
radkoff26 <radko.slava@mail.ru>
2023-05-05 20:08:26 +0300

    reorganize dependencies in ApiModule

--- app/src/main/java/com/example/flocator/di/ApiModule.kt
+++ app/src/main/java/com/example/flocator/di/ApiModule.kt
@@ -3,8 +3,7 @@ package com.example.flocator.di
 import com.example.flocator.common.config.Constants
 import com.example.flocator.main.api.ClientAPI
 import com.example.flocator.main.api.GeocoderAPI
-import com.example.flocator.main.deserializers.AddressDeserializer
-import com.example.flocator.main.data.response.AddressResponse
+import com.google.gson.Gson
 import com.google.gson.GsonBuilder
 import dagger.Module
 import dagger.Provides
@@ -19,33 +18,28 @@ import javax.inject.Singleton
 @InstallIn(SingletonComponent::class)
 @Module
 object ApiModule {
+    @Provides
+    @Singleton
+    fun provideGson(): Gson =
+        GsonBuilder()
+            .setLenient()
+            .create()
 
     @Provides
     @Singleton
-    fun provideClientAPI(): ClientAPI {
-        val gson = GsonBuilder()
-            .setLenient()
-            .create()
-        val retrofit = Retrofit.Builder()
+    fun provideRetrofit(gson: Gson): Retrofit =
+        Retrofit.Builder()
             .baseUrl(Constants.BASE_URL)
             .addConverterFactory(GsonConverterFactory.create(gson))
             .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
             .build()
-        return retrofit.create()
-    }
+
 
     @Provides
     @Singleton
-    fun provideGeocoderAPI(): GeocoderAPI {
-        val gson = GsonBuilder()
-            .registerTypeAdapter(AddressResponse::class.java, AddressDeserializer())
-            .setLenient()
-            .create()
-        val retrofit = Retrofit.Builder()
-            .baseUrl("https://geocode-maps.yandex.ru/")
-            .addConverterFactory(GsonConverterFactory.create(gson))
-            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
-            .build()
-        return retrofit.create()
-    }
+    fun provideClientAPI(retrofit: Retrofit): ClientAPI = retrofit.create()
+
+    @Provides
+    @Singleton
+    fun provideGeocoderAPI(retrofit: Retrofit): GeocoderAPI = retrofit.create()
 }
