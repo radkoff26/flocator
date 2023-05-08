2b3582171eec0377af15f43b978f2a523482d565
GenryEden <a926788@gmail.com>
2023-05-08 19:07:17 +0300

    remade UserInfo.kt to serialize Timestamp

--- app/src/main/java/com/example/flocator/common/storage/storage/user/info/UserInfo.kt
+++ app/src/main/java/com/example/flocator/common/storage/storage/user/info/UserInfo.kt
@@ -1,7 +1,26 @@
 package com.example.flocator.common.storage.storage.user.info
 
 import com.google.gson.annotations.SerializedName
+import kotlinx.serialization.Contextual
+import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.KSerializer
+import kotlinx.serialization.Serializer
+import kotlinx.serialization.descriptors.SerialDescriptor
+import kotlinx.serialization.encoding.Decoder
+import kotlinx.serialization.encoding.Encoder
 import java.sql.Timestamp
+
+
+@OptIn(ExperimentalSerializationApi::class)
+@Serializer(forClass = Timestamp::class)
+object TimestampSerializer: KSerializer<Timestamp> {
+    override fun serialize(encoder: Encoder, value: Timestamp) {
+        encoder.encodeString(value.toString())
+    }
+    override fun deserialize(decoder: Decoder): Timestamp {
+        return Timestamp.valueOf(decoder.decodeString())
+    }
+}
 
 @kotlinx.serialization.Serializable
 data class UserInfo(
@@ -15,10 +34,11 @@ data class UserInfo(
     val login: String,
     @SerializedName("avatarUrl")
     val avatarUri: String?,
+    @kotlinx.serialization.Serializable(with = TimestampSerializer::class)
     @SerializedName("birthDate")
     val birthDate: Timestamp,
     @SerializedName("blockedUsers")
-    val blockedUsers: List<Boolean>
+    val blockedUsers: List<Long>
 ) {
 
     companion object {
@@ -27,12 +47,10 @@ data class UserInfo(
             "",
             "",
             "",
-            null
+            null,
+            Timestamp(0),
+            List(0) { _ -> 0L }
         )
     }
-
-}
-
-object TimestampSerializer: KSerializer<> {
 
 }
