ae4673f3a4f66aa6eb2db3fb8b2456f5057b336d
radkoff26 <radko.slava@mail.ru>
2023-05-25 21:17:01 +0300

    remove tests, constraint toolbar

--- app/src/androidTest/java/com/example/flocator/radko_test/data/Credentials.kt
+++
@@ -1,3 +0,0 @@
-package com.example.flocator.radko_test.data
-
-open class Credentials(val login: String, val password: String)
--- app/src/androidTest/java/com/example/flocator/radko_test/data/TestUserCredentials.kt
+++
@@ -1,3 +0,0 @@
-package com.example.flocator.radko_test.data
-
-object TestUserCredentials: Credentials("radkoff", "qwertyuiop")
