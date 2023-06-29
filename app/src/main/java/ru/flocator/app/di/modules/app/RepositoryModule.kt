5de7ab7800638a2348091f43c94fdf3824acc57f
radkoff26 <radko.slava@mail.ru>
2023-06-30 00:00:59 +0300

    fix: validate app Dagger-modules

--- app/src/main/java/ru/flocator/app/di/modules/app/RepositoryModule.kt
+++ app/src/main/java/ru/flocator/app/di/modules/app/RepositoryModule.kt
@@ -5,15 +5,10 @@ import dagger.Module
 import dagger.Provides
 import ru.flocator.cache.global.PhotoCacheManager
 import ru.flocator.core_api.api.AppRepository
-import ru.flocator.core_connection.live_data.ConnectionLiveData
 import ru.flocator.core_data_store.point.UserLocationPoint
 import ru.flocator.core_data_store.user.data.UserCredentials
 import ru.flocator.core_data_store.user.info.UserInfo
 import ru.flocator.core_database.ApplicationDatabase
-import ru.flocator.feature_auth.internal.data_source.AuthenticationApi
-import ru.flocator.feature_main.internal.data_source.ClientAPI
-import ru.flocator.feature_main.internal.data_source.GeocoderAPI
-import ru.flocator.feature_settings.internal.data_source.SettingsAPI
 import javax.inject.Singleton
 
 @Module
@@ -22,30 +17,18 @@ class RepositoryModule {
     @Provides
     @Singleton
     fun provideMainRepository(
-        clientAPI: ClientAPI,
-        geocoderAPI: GeocoderAPI,
-        userApi: ru.flocator.feature_community.internal.data_source.UserApi,
-        authenticationApi: AuthenticationApi,
         applicationDatabase: ApplicationDatabase,
         userLocationDataStore: DataStore<UserLocationPoint>,
         userCredentialsStore: DataStore<UserCredentials>,
         userInfoStore: DataStore<UserInfo>,
-        photoCacheManager: PhotoCacheManager,
-        settingsAPI: SettingsAPI,
-        connectionLiveData: ConnectionLiveData
+        photoCacheManager: PhotoCacheManager
     ): AppRepository {
         return AppRepository(
-            clientAPI,
-            geocoderAPI,
-            userApi,
-            authenticationApi,
             applicationDatabase,
             userLocationDataStore,
             userCredentialsStore,
             userInfoStore,
-            photoCacheManager,
-            settingsAPI,
-            connectionLiveData
+            photoCacheManager
         )
     }
 }
